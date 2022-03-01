22a890cda86495381282a496aadbb82da1da0906
Shion Ichikawa <shion1305@gmail.com>
2022-03-02 06:46:26 +0900

    [IMPLEMENT] easy data reader in DataReader.java

--- src/main/java/com/shion1305/route/io/DataReader.java
+++ src/main/java/com/shion1305/route/io/DataReader.java
@@ -1,18 +1,18 @@
 package com.shion1305.route.io;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shion1305.route.object.AccessData;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class DataReader {
-    public static void readData(){
-        try(FileInputStream stream=new FileInputStream("")){
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static AccessData[] readData(String fileName) throws IOException {
+        try (FileInputStream stream = new FileInputStream("datasets/" + fileName)) {
+            ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            return mapper.readValue(stream, AccessData[].class);
         }
     }
-
 }
