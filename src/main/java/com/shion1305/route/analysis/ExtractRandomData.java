cd98dafc8f2f172bf569dd8d7984da148380ec68
Shion Ichikawa <shion1305@gmail.com>
2022-03-05 07:00:50 +0900

    [CREATE] ExtractRandomData.java which searches AccessData containing specific string

---
+++ src/main/java/com/shion1305/route/analysis/ExtractRandomData.java
@@ -0,0 +1,25 @@
+package com.shion1305.route.analysis;
+
+import com.shion1305.route.io.DataReader;
+import com.shion1305.route.object.AccessData;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ExtractRandomData {
+    public static void main(String[] args) throws IOException {
+        List<AccessData> data = new ArrayList<>();
+        for (String file : new File("datasets").list()) {
+            data.addAll(DataReader.readData(file));
+        }
+        Scanner scanner=new Scanner(System.in);
+        while (scanner.hasNextLine()){
+            String query= scanner.nextLine();
+
+        }
+
+    }
+}
