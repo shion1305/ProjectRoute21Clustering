0fa08b553066e9e37bbcfcef288d8eecfa006430
Shion Ichikawa <shion1305@gmail.com>
2022-03-05 03:02:01 +0900

    [TEST CODE] changes in test code

--- src/main/java/com/shion1305/route/io/Main.java
+++ src/main/java/com/shion1305/route/io/Main.java
@@ -3,20 +3,23 @@ package com.shion1305.route.io;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shion1305.route.object.AccessData;
+import com.shion1305.route.process.Grouper;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.*;
 
 public class Main {
-    static HashMap<String, AccessData> dataBySIP;
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
         ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         AccessData[] data = mapper.readValue(new File("datasets/data2021-1210.json"), AccessData[].class);
-        System.out.println(data.length);
-        System.out.println(data[0].sourceData);
-        dataBySIP = new HashMap<>();
+        for (AccessData d : data) {
+            d.sourceData.headers.keySet().forEach(s -> System.out.println(s));
+        }
+        HashMap<String, ArrayList<AccessData>> d = Grouper.groupByPath(data);
+        for (String p : d.keySet()) {
 
+        }
     }
 }
