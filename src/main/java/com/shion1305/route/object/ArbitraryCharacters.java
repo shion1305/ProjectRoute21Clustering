0fc9d5de4e7981bcf5d95cb596009a51d8ad088c
Shion Ichikawa <shion1305@gmail.com>
2022-03-25 04:15:53 +0900

    [IMPLEMENT] implement and fixes in ArbitraryCharacters.java

--- src/main/java/com/shion1305/route/object/ArbitraryCharacters.java
+++ src/main/java/com/shion1305/route/object/ArbitraryCharacters.java
@@ -1,9 +1,21 @@
 package com.shion1305.route.object;
 
 public class ArbitraryCharacters implements TextGroup {
+    public enum Type {
+        NUMBER, CHARACTER, STRING
+    }
+
     String text;
+    Type type;
 
     public ArbitraryCharacters(String text) {
+        if (text.matches("^\\d+$")) {
+            this.type = Type.NUMBER;
+        } else if (text.matches("^\\[A-Za-z]$")) {
+            this.type = Type.CHARACTER;
+        } else {
+            this.type = Type.STRING;
+        }
         this.text = text;
     }
 
