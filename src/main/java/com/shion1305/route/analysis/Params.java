c1618251e769c5d529501753726b4eaa1342b3c4
Shion Ichikawa <shion1305@gmail.com>
2022-03-28 16:17:48 +0900

    [IMPLEMENT] implement getters and setters in Params.java

--- src/main/java/com/shion1305/route/analysis/Params.java
+++ src/main/java/com/shion1305/route/analysis/Params.java
@@ -9,4 +9,46 @@ public class Params implements Serializable {
     public ArrayList<String> ips = new ArrayList<>();
     public Date first;
     public Date last;
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public ArrayList<String> getIps() {
+        return ips;
+    }
+
+    public void setIps(ArrayList<String> ips) {
+        this.ips = ips;
+    }
+
+    public Date getFirst() {
+        return first;
+    }
+
+    public void setFirst(Date first) {
+        this.first = first;
+    }
+
+    public Date getLast() {
+        return last;
+    }
+
+    public void setLast(Date last) {
+        this.last = last;
+    }
+
+    @Override
+    public String toString() {
+        return "Params{" +
+                "count=" + count +
+                ", ips=" + ips +
+                ", first=" + first +
+                ", last=" + last +
+                '}';
+    }
 }
